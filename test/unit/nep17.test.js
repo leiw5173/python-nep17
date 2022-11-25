48c025c06e74f7bf285b10c106c3602b152adecc
Sam <73590328+leiw5173@users.noreply.github.com>
2022-11-25 17:36:43 +0800

    update test part 1st time

--- test/unit/nep17.test.js
+++ test/unit/nep17.test.js
@@ -3,13 +3,32 @@ const Neon = require("@cityofzion/neon-core")
 const fs = require("fs")
 const assert = require("assert")
 
-describle("NEP17 Token Test", function(){
+describe("NEP17 Token Test", function () {
     const TIME_CONSTANT = 4000
-    let nep, network
+    const AMOUNT = 1000
+    const URL = "http://127.0.0.1:50012"
+    const TO_ADDRESS = "NR8isKNEYB92CxmxzD6GEH3ZgkCnKpY7fg"
+    const CONTRACT_HASH = "0x1488ce22ded38a0b3a2df68f6ea786589a687c71"
+    let network, networkMagic, privateKey, signer
 
-    beforeEach(async function (){
-        const targetNetwork = sdk.types.NetworkOption.LocalNet
+    beforeEach(async function () {
+        network = JSON.parse(fs.readFileSync("default.neo-express").toString())
+        privateKey = network.wallets[0].accounts[0]["private-key"]
+        signer = new Neon.wallet.Account(privateKey)
+        networkMagic = network["magic"]
+    })
 
-        nep = await new sdk.
+    describe("Get right initial set up", function () {
+        it("the symbol should be TEST", async function () {
+            const txid = await sdk.helpers.variableInvoke(
+                URL,
+                networkMagic,
+                CONTRACT_HASH,
+                "symbol",
+                []
+            )
+            const res = await sdk.helpers.txDidComplete(URL, txid, true)
+            assert(Neon.u.base642utf8(res[0].value) == "TEST", "Symbol return wrong")
+        })
     })
 })
