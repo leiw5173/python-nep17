c47c10a91eff9022675619638578864cdc420a51
Sam <leiw5173@gmail.com>
2022-11-27 18:02:51 +0800

    update balanceOf script with props function

--- scripts/balanceOf.js
+++ scripts/balanceOf.js
@@ -1,16 +1,21 @@
-const { rpc, sc, u } = require("@cityofzion/neon-js")
+const { CONST, sc, u } = require("@cityofzion/neon-js")
 const Neon = require("@cityofzion/neon-js")
 const { helpers } = require("@cityofzion/props")
 
 const url = "https://testnet2.neo.coz.io:443"
 const address = "NQ8uxEgW8S8AEMKSXXXVDoEHEX9a1xiowA"
 const contractScript = "0x5fdb2552f6512f6ed60b6dc34c3c7fc5d756bef7"
-
-const rpcClient = new rpc.RPCClient(url)
+const networkMagic = CONST.MAGIC_NUMBER.TestNet
 
 async function balanceOf(address) {
     const params = [sc.ContractParam.hash160(address)]
-    const res = await rpcClient.invokeFunction(contractScript, "balanceOf", params)
+    const res = await helpers.variableInvoke(
+        url,
+        networkMagic,
+        contractScript,
+        "balanceOf",
+        params
+    )
     return res
 }
 
@@ -21,7 +26,7 @@ async function symbol() {
 }
 
 balanceOf(address)
-    .then((r) => console.log(r.stack[0].value))
+    .then((r) => console.log(r[0].value))
     .catch((e) => console.log(e))
 
 symbol()
